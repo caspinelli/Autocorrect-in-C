e34f754c22a995ec5be1d3b53f7aa1df2ce88f1c
Cooper Spinelli <nellymonster99@gmail.com>
2016-11-06 15:40:03 -0500

    Yeah

--- autocorrect.c
+++ autocorrect.c
@@ -141,7 +141,7 @@ trie_t follow_word(trie_t triePointer, char* wordGiven) {
 //////////////////////
 
 linked_t linked_create() {
-	struct linked newLinked = malloc(sizeof(struct linked)); // newLinked is a pointer to memory that is the size of a linked
+	linked_t newLinked = malloc(sizeof(struct linked)); // newLinked is a pointer to memory that is the size of a linked
 	newLinked->head = NULL; // follow the pointer and make the head of the linked NULL
 	return newLinked; // return the pointer to the linked
 }
