dc33aa877be1c1aebaecf71161d040c2e259b17e
Cooper Spinelli <nellymonster99@gmail.com>
2016-11-07 02:08:48 -0500

    Yeah

--- autocorrect.c
+++ autocorrect.c
@@ -224,7 +224,6 @@ void linked_append(linked_t s, trie_t item, char* iterativeBuild) {
 	int h = 0;
 	for (h; h < strlen(iterativeBuild); h++) {
 		new_node->iterativeBuild[h] = iterativeBuild[h];
-		printf("screwy?");
 	}
 	new_node->next = NULL;
 	struct linked_node* curr = s->head;
@@ -233,6 +232,7 @@ void linked_append(linked_t s, trie_t item, char* iterativeBuild) {
 	}
 	curr = new_node;
 	struct linked_node* should = s->head;
+	printf("%p\n", curr);
 	printf("%p", should);
 }
 
