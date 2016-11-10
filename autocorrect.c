85de1ec2c609558111407a74fdc84994f647a34f
Cooper Spinelli <nellymonster99@gmail.com>
2016-11-10 00:47:12 -0500

    Yeah

--- autocorrect.c
+++ autocorrect.c
@@ -72,10 +72,30 @@ void main() {
 // Autocorrect Functions //
 ///////////////////////////
 
+int numberMin(int one, int two, int three) {
+	if (one >= two && one >= three) {
+		return one;
+	} else if (two >= one && two >= three) {
+		return two;
+	} else {
+		return three;
+	}
+}
+
+int arrayMin(int* arrayToCheck, int size) {
+	int min = arrayToCheck[0];
+	int w = 1;
+	for (w; w < size; w++) {
+		if (arrayToCheck[w] < min) {
+			min = arrayToCheck[w];
+		}
+	}
+	return min;
+}
 
 void correct(trie_t triePointer, char* wordGiven, int maxEdit) {
 	linked_t bfsLinked = linked_create();
-	int givenLength = strlen(wordGiven) + maxEdit;
+	int givenLength = strlen(wordGiven) + 1;
 	int* startingMatrix = malloc(sizeof(int) * givenLength);
 	int i = 0;
 	for (i; i < givenLength; i++) {
@@ -93,14 +113,155 @@ void correct(trie_t triePointer, char* wordGiven, int maxEdit) {
 		}
 		wordSaver[r] = '\0';
 		trie_t nodeSaver = linked_pop(bfsLinked);
+		int g = 0;
+		for (g; g < sizeof(nodeSaver->next)/sizeof(nodeSaver->next[0]); g++) {
+			if (nodeSaver->next[g] != NULL) {
+				trie_t new_node = nodeSaver->next[g];
+				char buildingLetters[strlen(wordSaver) + 2];
+				int p = 0;
+				for (p; p < strlen(wordSaver); p++) {
+					buildingLetters[p] = wordSaver[p];
+				}
+				buildingLetters[p] = nodeSaver->next[g]->character;
+				buildingLetters[p+1] = '\0';
 
+				//New Matrix Building
 
+				if (fetchedMatrix == NULL) {
+					// Check whether to print
+					if (new_node->frequency > 0) {
+						int x = 0;
+						for (x; x < strlen(buildingLetters); x++) {
+							printf("%c", buildingLetters[x]);
+						}
+					}
+					linked_append(stack, new_node, buildingLetters, NULL);
+				} else {
+					// Make matrix
+					int* newMatrix = malloc(sizeof(int) * givenLength);
+					newMatrix[0] = strlen(buildingLetters);
+					int buildingLength = strlen(buildingLetters);
+					int b = 0;
+					for (b; b < givenLength - 1; b++) {
+						int cost;
+						if (buildingLetters[buildingLength - 1] == wordGiven[b] || (buildingLength > 1 && buildingLetters[buildingLength - 1] == wordGiven[b - 1] && buildingLetters[buildingLength - 2] == wordGiven[b])) {
+							cost = 0;
+						} else {
+							cost = 1;
+						}
+						newMatrix[b+1] = numberMin(fetchedMatrix[b+1] + 1, newMatrix[b] + 1, fetchedMatrix[b] + cost);
+					}
+
+					// Check whether to print
+					if (newMatrix[givenLength - 1] <= maxEdit) && new_node->frequency > 0) {
+						int x = 0;
+						for (x; x < strlen(buildingLetters); x++) {
+							printf("%c", buildingLetters[x]);
+						}
+					}
+
+					// Check whether to initiate complete
+					if (newMatrix[givenLength - 1] == 0) {
+						linked_append(stack, new_node, buildingLetters, NULL);
+					} else if (arrayMin(newMatrix, givenLength) <= maxEdit) { // Check whether to add back the linked
+						linked_append(stack, new_node, buildingLetters, newMatrix);
+					}
+				}
+
+			}
+		}
 	}
+}
 
-
+void complete(trie_t triePointer, char* wordGiven) {
+		linked_t stack = linked_create();
+		int i = 0;
+		char starterLetters[strlen(wordGiven) + 1];
+		int q = 0;
+		for (q; q < strlen(wordGiven); q++) {
+			starterLetters[q] = wordGiven[q];
+		}
+		starterLetters[q] = '\0';
+		linked_append(stack, starterNode, starterLetters, NULL);
+		while (linked_peek(stack) != NULL) {
+			int length = strlen(stack->head->iterativeBuild);
+			char wordSaver[length + 1];
+			int r = 0;
+			for (r; r < length; r++) {
+				wordSaver[r] = stack->head->iterativeBuild[r];
+			}
+			wordSaver[r] = '\0';
+			trie_t nodeSaver = linked_pop(stack); 
+			if (nodeSaver->frequency >= 1) {
+				int c = 0;
+				for (c; c < strlen(wordSaver); c++) {
+					printf("%c", wordSaver[c]);
+				}
+				printf("\n");
+			}
+			int g = 0;
+			for (g; g < sizeof(nodeSaver->next)/sizeof(nodeSaver->next[0]); g++) {
+				if (nodeSaver->next[g] != NULL) {
+					trie_t new_node = nodeSaver->next[g];
+					char buildingLetters[strlen(wordSaver) + 2];
+					int p = 0;
+					for (p; p < strlen(wordSaver); p++) {
+						buildingLetters[p] = wordSaver[p];
+					}
+					buildingLetters[p] = nodeSaver->next[g]->character;
+					buildingLetters[p+1] = '\0';
+					linked_append(stack, new_node, buildingLetters, NULL);
+				}
+			}
+		}
+		linked_destroy(stack);
+	}
 }
 
 
+/*
+void correct(trie_t triePointer, char* wordGiven, int maxEdit) {
+	linked_t bfsLinked = linked_create();
+	int givenLength = strlen(wordGiven) + 1;
+	int* startingMatrix = malloc(sizeof(int) * givenLength);
+	int i = 0;
+	for (i; i < givenLength; i++) {
+		startingMatrix[i] = i;
+	}
+	char startNull[1] = "\0";
+	linked_append(bfsLinked, triePointer, startNull, startingMatrix);
+	while (linked_peek(bfsLinked) != NULL) {
+		int* fetchedMatrix = bfsLinked->head->correctMatix;
+		int length = strlen(bfsLinked->head->iterativeBuild);
+		char wordSaver[length + 1];
+		int r = 0;
+		for (r; r < length; r++) {
+			wordSaver[r] = bfsLinked->head->iterativeBuild[r];
+		}
+		wordSaver[r] = '\0';
+		trie_t nodeSaver = linked_pop(bfsLinked);
+		int g = 0;
+		for (g; g < sizeof(nodeSaver->next)/sizeof(nodeSaver->next[0]); g++) {
+			if (nodeSaver->next[g] != NULL) {
+				trie_t new_node = nodeSaver->next[g];
+				char buildingLetters[strlen(wordSaver) + 2];
+				int p = 0;
+				for (p; p < strlen(wordSaver); p++) {
+					buildingLetters[p] = wordSaver[p];
+				}
+				buildingLetters[p] = nodeSaver->next[g]->character;
+				buildingLetters[p+1] = '\0';
+				
+				
+				//make new matrix
+
+
+				linked_append(stack, new_node, buildingLetters);
+			}
+		}
+	}
+
+}
 
 void complete(trie_t triePointer, char* wordGiven) {
 	trie_t starterNode = follow_word(triePointer, wordGiven);
@@ -150,6 +311,7 @@ void complete(trie_t triePointer, char* wordGiven) {
 }
 
 
+
 trie_t follow_word(trie_t triePointer, char* wordGiven) {
 	int h = 0;
 	for (h; h < strlen(wordGiven); h++) {
@@ -169,6 +331,7 @@ trie_t follow_word(trie_t triePointer, char* wordGiven) {
 	}
 	return NULL; // Returns null if there are no nodes following last node
 }
+*/
 
 
 ////////////////////
